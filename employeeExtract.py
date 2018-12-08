f36844564f9868e0e1fd8fcdadb90cb50129fae9
Alex Richards <arichards@go-iei.com>
2018-12-08 11:24:34 -0700

    add license schema and logic

--- employeeExtract.py
+++ employeeExtract.py
@@ -19,20 +19,36 @@ section = mydoc.getElementsByTagName('Detail')
 # 	- Write funciton to calculate years of expierence date
 # - Calculate the right licnese to use
 # - to accept multiple courses and certs
-# - 
+# - NOTE - IN ORDER TO MAKE LICENSE AND COURSES WORK - you gotta learn how to define classes so that you can have empty variables of them. Pointers are the issue here
 
 def appendStr(val, field, dataType):
 	if dataType == "int":
 		employee[field] = int(val)
 	elif dataType == "hireDate":
-		employee[field] = datetime.strptime(val.rstrip("T00:00:00"), '%Y-%m-%d') #Calculates date when called
+		employee[field] = formatDate(val) #Calculates date when called
 	elif dataType == "str":
 		employee[field].append(val)
+	elif dataType == "lic":
+		if field == "dateEarned":
+			newLicense[field] = formatDate(val)
+		elif field == "number":
+			newLicense[field] = int(val)
+		elif field == "type" or field == "state":
+			newLicense[field] = val
+		elif field == "dateExpired":
+			newLicense[field] == formatDate(val)
+			employee["licenses"].update({ newLicense["state"] + str(newLicense["number"]) : newLicense  })
+
+		
+
 
 def calculateYearsBetween(startDate):
 	today = datetime.now()
 	difference = relativedelta.relativedelta(today, startDate).years
 	return difference
+
+def formatDate(date):
+	return datetime.strptime(date.rstrip("T00:00:00"), '%Y-%m-%d').date()
 
 employee = {
 	"name": [],
@@ -40,6 +56,15 @@ employee = {
 	"hireDate": 0,
 	"priorYearsFirm": 0,
 	"priorYearsOther": 0,
+	"licenses": {}, 
+}
+
+newLicense = {
+	"number": 0, 
+	"type": "",
+	"dateEarned": 0,
+	"dateExpired": 0,
+	"state": ""
 }
 
 employeeInfo = {
@@ -47,12 +72,12 @@ employeeInfo = {
 	"DetailField_Title_Section_1": ["title", "str"],
 	"DetailField_HireDate_Section_1": ["hireDate", "hireDate"],
 	"DetailField_PriorYearsFirm_Section_1": ["priorYearsFirm", "int"], 
-	"DetailField_YearsOtherFirms_Section_1": ["priorYearsOther", "int"]
-	# "detail_Licenses_License": "",
-	# "detail_Licenses_Earned": "",
-	# "detail_Licenses_State": "",
-	# "detail_Licenses_Number": 0,
-	# "detail_Licenses_Expires": 0,
+	"DetailField_YearsOtherFirms_Section_1": ["priorYearsOther", "int"],
+	"detail_Licenses_License" : ["type", "lic"],
+	"detail_Licenses_Earned": ["dateEarned", "lic"],
+	"detail_Licenses_State": ["state", "lic"],
+	"detail_Licenses_Number": ["number", "lic"],
+	"detail_Licenses_Expires": ["dateExpired", "lic"], #Hook to signal end of newLicense object
 	# "detail_gridUDCol_Employees_Courses_custAgency": "", #Need to rewrite to accept multiple courses and certs
 	# "detail_gridUDCol_Employees_Courses_custCourseName": "",
 	# "detail_gridUDCol_Employees_Courses_custDate": "",
@@ -83,7 +108,8 @@ employee.update({"yearsAtConsor": calculateYearsBetween(employee["hireDate"])}) 
 
 employee.update({"totalYearsExp": employee["yearsAtConsor"] + employee["priorYearsOther"] + employee["priorYearsFirm"]})
 
-print(employee)
+
+print(employee["licenses"]["FL74796"]["dateEarned"])
 
 
 # with open('employeeInfo.csv', 'w') as csvfile:
