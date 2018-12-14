dcb9ba2aba9b37b572f14fad1bb5a4415a802b67
Alex Richards <arichards@go-iei.com>
2018-12-14 10:19:34 -0700

    write each object to CSV successfully

--- employeeExtract.py
+++ employeeExtract.py
@@ -26,14 +26,21 @@ section = mydoc.getElementsByTagName('Detail')
 # Split name into first last and middle - concat based on the previous todo (first + Last + Suffix for most) 
 
 
-with open('employeeInfo.csv', 'w', newline='') as csvfile:
-	fieldnames = ["name", "nameSuffix", "title", "hireDate", "priorYearsFirm", "priorYearsOther", "hireDate", "priorYearsFirm", "priorYearsOther", "licenses", "courses", "certifications", "education", "resumeIntro", "totalYearsExp"]
-	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-	writer.writeheader()
+# with open('employeeInfo.csv', 'w', newline='') as csvfile:
+# 	fieldnames = ["name", "nameSuffix", "title", "hireDate", "priorYearsFirm", "priorYearsOther", "hireDate", "priorYearsFirm", "priorYearsOther", "licenses", "courses", "certifications", "education", "resumeIntro", "totalYearsExp"]
+# 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# 	writer.writeheader()
 
 
 def formatDate(date):
 	return datetime.strptime(date.rstrip("T00:00:00"), '%Y-%m-%d').date() #May add to Employee
+
+def representsInt(input):
+	try:
+		int(input)
+		return True
+	except ValueError:
+		return False
 
 #base class Employee
 class Employee:
@@ -47,9 +54,10 @@ class Employee:
 			"priorYearsFirm": 0,
 			"priorYearsOther": 0,
 			"licenses": {}, # Write a function for employee to roll up all the displays for licenses into one string to put in csv
+			"licenseDisplay": "",
 			"courses": [],
 			"certifications": [], 
-			"education": [], 
+			"education": {},
 			"resumeIntro": [], 
 			"PELicenseCount": 0, 
 			"courseCount": 0,
@@ -57,7 +65,6 @@ class Employee:
 			"degreeCount": 0
 		}
 
-		self.licenseDisplay = ""
 		self.courseDisplay = ""
 
 	detailKey = {
@@ -78,6 +85,7 @@ class Employee:
 		"detail_gridUDCol_Employees_Courses_custCourseNumber": "cor",
 		"detail_gridUDCol_Employees_Certifications_custCertAgency": "cert",
 		"detail_gridUDCol_Employees_Certifications_custCertNumber": "cert",
+		"detail_gridUDCol_Employees_Certifications_custTitle": "cert",
 		"detail_gridUDCol_Employees_Certifications_custExpirationDate": "cert",
 		"detail_gridUDCol_Employees_Certifications_CustNoExpiration": "cert",
 		"detail_Education_Degree": "edu",
@@ -107,6 +115,10 @@ class Employee:
 		"detail_gridUDCol_Employees_Courses_custCourseName": "name",
 		"detail_gridUDCol_Employees_Courses_custDate": "dateTaken",
 		"detail_gridUDCol_Employees_Courses_custCourseNumber": "number",
+		"detail_Education_Degree": "degree",
+		"detail_Education_Specialty": "specialty",
+		"detail_Education_Institution": "school",
+		"detail_Education_Year": "gradYear",
 	}
 
 	def formatData(self, data, dataType):
@@ -132,7 +144,19 @@ class Employee:
 		for license in self.data["licenses"]:
 			if self.data["licenses"][license].displayString == None:
 				return
-			self.licenseDisplay += self.data["licenses"][license].displayString
+			self.data["licenseDisplay"] += self.data["licenses"][license].displayString
+
+	def removeTrailingComma(self):
+		if self.data["licenseDisplay"].endswith(", "):
+			self.data["licenseDisplay"] = self.data["licenseDisplay"][: -2]
+
+	def formatEducation(self):
+		for edu in self.data["education"]:
+			if representsInt(edu) == True:
+				edu += edu + '\n'
+				str1 = ", ".join(self.data["education"])
+				print(str1)
+
 
 
 allEmployees = dict()
@@ -150,9 +174,6 @@ class License(Employee):
 		self.isMain = False
 		self.isExpired = False
 		self.displayString = None 
-
-	def formatLicense(self):
-		print(self.data)
 
 	def formatType(self, licType):
 		if licType == "Professional Engineer":
@@ -176,6 +197,8 @@ class License(Employee):
 			if self.isMain == True:
 				self.displayString = self.data["state"] + " #" + self.data["number"]
 			self.displayString = self.data["state"] + ", "
+
+	
 		
 class Course(Employee):
 
@@ -188,6 +211,20 @@ class Course(Employee):
 		}
 		self.isExpired: False
 		self.displayString: None
+
+
+class Degree(Employee):
+
+	def __init__(self):
+		self.data = {
+			"degree": "",
+			"specialty": "",
+			"school": "",
+			"gradYear": 0
+		}
+
+
+
 
 
 newCert = {
@@ -225,7 +262,7 @@ for element in section[:]:
 				# 	allEmployees[objectName].data["courseCount"] += 1
 				# 	allEmployees[objectName].data["courses"][allEmployees[objectName].data["courseCount"]] = Course()
 				# 	allEmployees[objectName].data["courses"][allEmployees[objectName].data["courseCount"]].data[Employee.objectKey[key]] = element.getAttribute(key)
-				# #need to deal with instances where the agency is blank - need to
+				# #need to deal with instances where the agency is blank as the hook is missed- need to
 				# # 1. recognize that and object already exists and needs to be iterated 
 				# # 2. Iterate the counter
 				# # 3. Create the new course object
@@ -236,7 +273,12 @@ for element in section[:]:
 			if Employee.detailKey[key] == "cert":
 				allEmployees[objectName].data["certifications"].append(element.getAttribute(key)) 
 			if Employee.detailKey[key] == "edu":
-				allEmployees[objectName].data["education"].append(element.getAttribute(key))
+				if Employee.objectKey[key] == "degree":
+					allEmployees[objectName].data["degreeCount"] += 1
+					allEmployees[objectName].data["education"][allEmployees[objectName].data["degreeCount"]] = Degree()
+					allEmployees[objectName].data["education"][allEmployees[objectName].data["degreeCount"]].data[Employee.objectKey[key]] = element.getAttribute(key)
+
+				allEmployees[objectName].data["education"][allEmployees[objectName].data["degreeCount"]].data[Employee.objectKey[key]] = element.getAttribute(key)
 			if Employee.detailKey[key] == "res":
 				allEmployees[objectName].data["resumeIntro"].append(element.getAttribute(key)) 
 
@@ -248,10 +290,13 @@ for emp in allEmployees:
 # Rollup licenses for employee
 for emp in allEmployees:
 	allEmployees[emp].rollupLicenses()
+	allEmployees[emp].calculateYearsExp()
+	allEmployees[emp].removeTrailingComma()
+	# allEmployees[emp].formatEducation()
 
 
-for emp in allEmployees:
-	print(allEmployees[emp].licenseDisplay)
+# for emp in allEmployees:
+# 	print(allEmployees[emp].data)
 
 # 	for course in allEmployees[emp].data["courses"]:
 # 		print(allEmployees[emp].data["courses"][course].data)
@@ -265,8 +310,10 @@ for emp in allEmployees:
 
 # Write to iterate over the dictionary of workers 
 
-# with open('employeeInfo.csv', 'w', newline='') as csvfile:
-# 	fieldnames = ["name", "nameSuffix", "title", "hireDate", "priorYearsFirm", "priorYearsOther", "licenses", "courses", "certifications", "education", "resumeIntro", "totalYearsExp"]
-# 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-# 	writer.writerow(Worker.data)
+with open('employeeInfo.csv', 'w', newline='') as csvfile:
+	fieldnames = ["name", "nameSuffix", "title", "hireDate", "priorYearsFirm", "priorYearsOther", "licenseDisplay", "courses", "certifications", "education", "resumeIntro", "totalYearsExp", "degreeCount", "courseCount", "PELicenseCount", "certCount", "licenses"]
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+	for emp in allEmployees:
+		writer.writerow(allEmployees[emp].data)
 
